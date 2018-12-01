5fdc29e1a8978fece9b49859931225b963fe03df
Steve Mostovoy <stevemostovoysm@gmail.com>
2018-12-01 13:48:08 -0800

    Change chicken spawning

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour {
     public Transform chicken;
+    private Vector2 backLeftCorner = new Vector2(-15, 5);
+    private Vector2 forwardRightCorner = new Vector2(15, 75);
 
     void Start() {
         for (int i = 0; i < 20; i++) {
@@ -12,9 +14,9 @@ public class LevelController : MonoBehaviour {
             rotation.w = 0;
             Instantiate(chicken,
                 new Vector3(
-                    Random.Range(5, 15),
+                    Random.Range(this.backLeftCorner.x, this.forwardRightCorner.x),
                     0,
-                    Random.Range(5, 15)),
+                    Random.Range(this.backLeftCorner.y, this.forwardRightCorner.y)),
                 rotation);
         }
     }
